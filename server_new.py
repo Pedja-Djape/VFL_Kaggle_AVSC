ad55ef65b5fd0be53ef51f0f37748783bd15e344
Pedja-Djape <pmuratov1015@gmail.com>
2023-01-03 22:49:49 -0500

    saving new pickled dataset everytime server starts

--- server_new.py
+++ server_new.py
@@ -4,33 +4,29 @@ from utils import ShoppersDataset,save_data
 import time
 
 from torch.optim import Adam
+from torch.utils.data import DataLoader
 import flwr as fl
 
 
 if __name__ == "__main__":
     BATCH_SIZE = 32
     NUM_CLIENTS = 3
-    NUM_ROUNDS = 30000
-    outfile = 'data.pt'
+    NUM_ROUNDS = 5000
+    outfile = './data.pt'
     infile = './data.pt'
 
-    DATA = None
-    with open(infile,'rb') as f:
-        try:
-            DATA = pickle.load(f)
-        except EOFError:
-            DATA = save_data(data_path='../data/train_data.csv', batch_size=BATCH_SIZE, outfile=outfile)
-
+    
+    DATA = save_data(data_path='../data/train_data.csv', batch_size=BATCH_SIZE, outfile=outfile)
 
     if 'batch_size' not in DATA or DATA['batch_size'] != BATCH_SIZE:
         print(f"Saving data with new batch_size: {BATCH_SIZE}")
         DATA = save_data(data_path='../data/train_data.csv', batch_size=BATCH_SIZE, outfile=outfile)
     
-
+    
     strategy = stgy.SplitVFL(
         num_clients=NUM_CLIENTS, 
         batch_size=BATCH_SIZE, 
-        dim_input= 18, # 4 outputs for three clients
+        dim_input= 18, # 6 outputs for three clients
         labels = DATA['labels']
     )
 
