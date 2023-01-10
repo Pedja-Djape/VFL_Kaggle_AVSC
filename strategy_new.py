38a80df4abbadc11e1943cd90e86935a020f429b
Pedja-Djape <pmuratov1015@gmail.com>
2023-01-10 11:14:11 -0500

    added test fxnality and evaluation of f1-score

--- strategy_new.py
+++ strategy_new.py
@@ -20,13 +20,13 @@ from flwr.server.client_proxy import ClientProxy
 
 import numpy as np
 import model
-
+from sklearn.metrics import f1_score
 
 from torch import optim
 from torch.nn import BCELoss
 import torch
 
-from pickle import dump
+from pickle import dump,loads
 accs = []
 
 torch.manual_seed(0)
@@ -34,7 +34,7 @@ def get_parameters(net) -> List[np.ndarray]:
     return [val.cpu().numpy() for _, val in net.state_dict().items()]
 
 class SplitVFL(Strategy):
-    def __init__(self, num_clients, batch_size, dim_input, labels, lr=1e-6):
+    def __init__(self, num_clients, batch_size, dim_input, train_labels, test_labels, lr=1e-6):
         super(SplitVFL, self).__init__()
 
         self.batch_size = batch_size
@@ -42,17 +42,73 @@ class SplitVFL(Strategy):
         self.dim_input = dim_input
         self.lr = lr
         self.criterion = BCELoss()
-        self.labels = torch.tensor(labels)
-        self.lower_label_idx = 0
-        self.num_correct = 0
+        self.train_labels = torch.tensor(train_labels)
+        self.test_labels  = torch.tensor(test_labels)
+
+        self.train_lwr_idx = 0
+        self.test_lwr_idx = 0
+
+        self.train_acc = []
+        self.test_acc = []
+
+        self.train_correct = 0
+        self.test_correct = 0
+
+        self.test_f1 = []
+        self.test_f1_accum = 0
 
         self.model = None
         self.client_tensors = None
         self.clients_map = None
         self.optim = None
+
+        
         
     def __repr__(self) -> str:
         return "VerticalFed"
+    
+    def update_label_index(self,test=False):
+        def update(lwr,len_ls):
+            l,u = None,None
+            if lwr < len_ls:
+                if len_ls - lwr < self.batch_size:
+                    u = len_ls
+                    l = lwr
+                else:
+                    u = lwr + self.batch_size
+                    l = lwr
+            else:
+                l = 0
+                u = l + self.batch_size
+                if test:
+                    self.test_acc += [self.test_correct / len(self.test_labels)]
+                    self.test_correct = 0
+                    self.test_f1 += [self.test_f1_accum / len(self.test_labels)]
+                    self.test_f1_accum
+                else:
+                    self.train_acc += [self.train_correct / len(self.train_labels)]
+                    self.train_correct = 0
+            return l,u
+            
+        
+        idx_u = None
+        if not test:
+            self.train_lwr_idx, idx_u = update(lwr=self.train_lwr_idx, len_ls=len(self.train_labels))
+        else:
+            self.test_lwr_idx, idx_u = update( lwr=self.test_lwr_idx,  len_ls=len(self.test_labels))
+        # if self.train_lwr_idx < len(self.train_labels):
+        #     # last batch has less than batch_size samples
+        #     if len(self.train_labels) - self.train_lwr_idx < self.batch_size:
+        #         idx_u = len(self.train_labels)
+        #     else:
+        #         # update upper bound normally
+        #         idx_u = self.train_lwr_idx + self.batch_size
+        # else:
+        #     # just passed final batch
+        #     self.train_lwr_idx = 0
+        #     idx_u = self.train_lwr_idx + self.batch_size
+        
+        return idx_u
     
     def initialize_parameters(
         self, client_manager: ClientManager
@@ -76,12 +132,13 @@ class SplitVFL(Strategy):
         config = { 'server_round': server_round }
         fit_ins = FitIns(parameters, config)
         # return list of tuples of client and fit instructions
-        # fit instructions are just global mode parameters and config.
+        # fit instructions are just global model parameters and config.
         return [ (client, fit_ins) for client in clients]
 
     # below works for clients sending one batch at a time
     def __convert_results_to_tensor(self,
         results: List[Tuple[ClientProxy, FitRes]],
+        test=False
         ):
         numpy_input = np.empty((self.num_clients, self.batch_size, self.dim_input // self.num_clients))
 
@@ -90,19 +147,24 @@ class SplitVFL(Strategy):
 
         for i, (client, fit_response) in enumerate(results):
             # get client embeddings as numpy arrays
-            client_embeddings = parameters_to_ndarrays(fit_response.parameters)
+            client_embeddings = None
+            if not test:
+                client_embeddings = parameters_to_ndarrays(fit_response.parameters)
+            else:
+                client_embeddings = loads(fit_response.metrics['test_embeddings'])
             for j, embeds in enumerate(client_embeddings):
                 numpy_input[i,j,:] = embeds.astype(np.float32)
             
             clients_map[client.cid] = i
             client_tensors.append(
-                torch.tensor(numpy_input[i],dtype=torch.float32,requires_grad=True)
+                torch.tensor(numpy_input[i],dtype=torch.float32,requires_grad=True if not test else False)
             )
 
         gbl_model_input = torch.cat(client_tensors,1)
 
-        self.client_tensors = client_tensors
-        self.clients_map = clients_map
+        if not test:
+            self.client_tensors = client_tensors
+            self.clients_map = clients_map
 
         return gbl_model_input
 
@@ -113,44 +175,50 @@ class SplitVFL(Strategy):
         failures: List[Union[Tuple[ClientProxy, FitRes], BaseException]],
     ) -> Tuple[Optional[Parameters], Dict[str, Scalar]]:
         
-        
+        # convert all intermediate results from clients to 
+        # input to the global model
         gbl_model_input = self.__convert_results_to_tensor(results=results)
         gbl_model_output = self.model(gbl_model_input)
 
-        idx_u = None
-        if self.lower_label_idx < len(self.labels):
-            if len(self.labels) - self.lower_label_idx < self.batch_size:
-                idx_u = len(self.labels)
-            else:
-                idx_u = self.lower_label_idx + self.batch_size
-        else:
-            self.lower_label_idx = 0
-            idx_u = self.lower_label_idx + self.batch_size
+        # upper idx
+        # idx_u = None
+        # # if the lower index is less than the number of labels
+        # if self.train_lwr_idx < len(self.train_labels):
+        #     # last batch has less than batch_size samples
+        #     if len(self.train_labels) - self.train_lwr_idx < self.batch_size:
+        #         idx_u = len(self.train_labels)
+        #     else:
+        #         # update upper bound normally
+        #         idx_u = self.train_lwr_idx + self.batch_size
+        # else:
+        #     # just passed final batch
+        #     self.train_lwr_idx = 0
+        #     idx_u = self.train_lwr_idx + self.batch_size
 
-            global accs
-            accs.append(self.num_correct / len(self.labels))
-            self.num_correct = 0
-            with open('accs.pt','wb') as f:
-                dump(accs, f)
+        
+        idx_u = self.update_label_index()
 
         self.optim.zero_grad()
         loss = self.criterion(
             gbl_model_output,
-            self.labels[
-                self.lower_label_idx : idx_u
+            self.train_labels[
+                self.train_lwr_idx : idx_u
             ].float()
         )
         loss.backward()
         self.optim.step()
+
+        preds = torch.round(gbl_model_output)
         num_correct = (
-                torch.round(gbl_model_output) == self.labels[self.lower_label_idx : idx_u] 
+                preds == self.train_labels[self.train_lwr_idx : idx_u] 
             ).float().sum().squeeze().item()
 
-        self.num_correct += num_correct
-
-        self.lower_label_idx += self.batch_size
+        self.train_correct += num_correct
         
-        print(f'Accuracy: {num_correct / len(gbl_model_input)}, \tnum samples: {len(gbl_model_input)}')
+
+        self.train_lwr_idx += self.batch_size
+        
+        print(f'\ntrainAccuracy: {num_correct / len(gbl_model_input)}, \tnum samples: {len(gbl_model_input)}\n')
         return (ndarrays_to_parameters(
             get_parameters(self.model)
         ), {'loss': str(loss.item()), })
@@ -189,8 +257,34 @@ class SplitVFL(Strategy):
         results: List[Tuple[ClientProxy, EvaluateRes]],
         failures: List[Union[Tuple[ClientProxy, FitRes], BaseException]],
     ) -> Tuple[Optional[float], Dict[str, Scalar]]:
-        # print("\n\n\nAGGREGATE_EVALUATE\n\n\n")
-        return (None, {'None': str(None)})
+
+        criterion = BCELoss()
+        
+        gbl_model_input = self.__convert_results_to_tensor(results,test=True)
+        
+        with torch.no_grad():
+            gbl_model_output = self.model(gbl_model_input).float()
+            idx_u = self.update_label_index(test=True)
+            
+            loss = criterion(
+                gbl_model_output,
+                self.test_labels[
+                    self.test_lwr_idx : idx_u
+                ].float()
+            )
+            preds = torch.round(gbl_model_output)
+            num_correct = (
+                preds == self.test_labels[self.test_lwr_idx : idx_u] 
+            ).float().sum().squeeze().item()
+
+            acc = num_correct / len(gbl_model_input)
+            f1 = f1_score(self.test_labels[self.test_lwr_idx : idx_u].numpy(), preds.numpy())
+
+            self.test_f1_accum += f1
+            self.test_lwr_idx += self.batch_size
+            self.test_correct += num_correct
+            
+        return (server_round, {'loss': str(loss.item()), 'accuracy': acc, 'f1': f1})
 
     
     def evaluate(
