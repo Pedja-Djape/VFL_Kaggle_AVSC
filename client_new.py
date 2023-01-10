ec5e32e302a90bd142b1aaaa02beddb4a9e65f60
Pedja-Djape <pmuratov1015@gmail.com>
2023-01-10 11:15:36 -0500

    added testing functionality

--- client_new.py
+++ client_new.py
@@ -7,11 +7,13 @@ from collections import OrderedDict
 from flwr.common import parameters_to_ndarrays
 DEVICE = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
+import pickle as pi
+
 
 
 
 class FlowerClient(fl.client.NumPyClient):
-    def __init__(self,cid, net, trainloader,optimizer):
+    def __init__(self,cid, net, trainloader,testloader, optimizer):
         super(FlowerClient,self).__init__()
         # client id
         self.cid = cid
@@ -20,6 +22,10 @@ class FlowerClient(fl.client.NumPyClient):
         # train iterator
         self.trainloader = trainloader
         self.trainiter = iter(trainloader)
+
+        self.test_loader = testloader 
+        self.testiter = iter(self.test_loader)
+
         # optimizer (Adamm, etc.)
         self.optimizer = optimizer
         # outputs from previous round
@@ -50,7 +56,17 @@ class FlowerClient(fl.client.NumPyClient):
     def evaluate(self, parameters, config):
         self.outputs.backward(torch.tensor(np.array(parameters)))
         self.optimizer.step()
-        return .0, 0, {}
+
+        try:
+            X = next(self.testiter)
+        except StopIteration:
+            self.testiter = iter(self.test_loader)
+            X = next(self.testiter)
+        
+        with torch.no_grad():
+            outputs = self.net(X.float()).numpy()
+
+        return 0., 0, {'test_embeddings': pi.dumps([ x for x in outputs])}
     
 if __name__ == "__main__":
     import argparse
@@ -68,15 +84,21 @@ if __name__ == "__main__":
     with open('data.pt','rb') as f:
         data = pickle.load(f)
     
-    dataloader = data['data'][int(args.cid)]
+    train_dataloader = data['data']['train'][int(args.cid)]
+    test_dataloader = data['data']['test'][int(args.cid)]
     
     torch.manual_seed(0)
-    model = model.Net(dataloader.dataset.X.shape[-1], 6)
+    model = model.Net(train_dataloader.dataset.X.shape[-1], 6)
 
     fl.client.start_numpy_client(
         server_address="127.0.0.1:8080",
         client=FlowerClient(
-            cid=str(args.cid), net = model, trainloader=dataloader, optimizer = Adam(model.parameters(), lr=1e-2))
+            cid=str(args.cid), 
+            net = model, 
+            trainloader=train_dataloader, 
+            testloader=test_dataloader, 
+            optimizer = Adam(model.parameters(), lr=1e-8)
+        )
     )
     
 
--- utils.py
+++ utils.py
@@ -17,10 +17,29 @@ class ShoppersDataset(Dataset):
     def __getitem__(self, index):
         return self.X[index,:]
 
+def create_train_test(df,cols, train_index, test_index, batch_size):
+
+    train_ds = ShoppersDataset(
+        df.loc[train_index][cols].copy().to_numpy()
+    )
+    test_ds = ShoppersDataset(
+        df.loc[test_index][cols].copy().to_numpy()
+    )
+    train_dl = DataLoader(dataset=train_ds, batch_size=batch_size, shuffle=False)
+    test_dl = DataLoader(dataset=test_ds, batch_size=batch_size, shuffle=False)
+    return train_dl,test_dl
+
 def load_datasets(data_path,batch_size):
     df = pd.read_csv(data_path)
 
-    targets = df['repeater'].values.reshape((-1,1))
+    targets = df['repeater']
+
+    train_index, test_index = train_test_split(df.index.values,test_size=0.1,shuffle=False)
+    # treat labels as dataloaders
+    train_labels_1, test_labels_1 = create_train_test(
+        df=df, cols=['repeater'], train_index=train_index, test_index=test_index, batch_size=batch_size
+    )
+
     df = df.drop('repeater',axis=1)
 
     comp_cols, brand_cols, cat_cols = [],[],[]
@@ -39,27 +58,25 @@ def load_datasets(data_path,batch_size):
         if cnt == -1:
             comp_cols.append(col)
     
-    comp_ds = df[comp_cols].copy()
-    cat_ds = df[cat_cols].copy()
-    brand_ds = df[brand_cols].copy()
     
-    comp_dl = DataLoader(
-        dataset=ShoppersDataset(comp_ds.to_numpy()),
-        batch_size=batch_size,
-        shuffle=False
-    )
-    cat_dl = DataLoader(
-        dataset=ShoppersDataset(cat_ds.to_numpy()),
-        batch_size=batch_size,
-        shuffle=False
-    )
-    brand_dl = DataLoader(
-        dataset=ShoppersDataset(brand_ds.to_numpy()),
-        batch_size=batch_size,
-        shuffle=False
-    )
 
-    return {'data': [comp_dl,cat_dl,brand_dl], 'labels': targets, 'batch_size': batch_size}
+    train_comp_dl, test_comp_dl = create_train_test(df, cols=comp_cols,  train_index=train_index, test_index=test_index, batch_size=batch_size)
+    train_cat_dl, test_cat_dl = create_train_test(  df, cols=cat_cols,   train_index=train_index, test_index=test_index, batch_size=batch_size)
+    train_brand_dl, test_brand_dl = create_train_test( df, cols=brand_cols, train_index=train_index, test_index=test_index, batch_size=batch_size)
+
+    
+    train_labels = targets.loc[train_index].values.reshape((-1,1))
+    test_labels  = targets.loc[test_index ].values.reshape((-1,1))
+
+
+    return {
+        'data': {
+            'train': [train_comp_dl,train_cat_dl,train_brand_dl], 
+            'test': [test_comp_dl,test_cat_dl,test_brand_dl]
+        },    
+        'train_labels': train_labels, 'test_labels': test_labels, 
+        'batch_size': batch_size
+    }
 
 def save_data(data_path,batch_size,outfile):
     data = load_datasets(
@@ -71,3 +88,10 @@ def save_data(data_path,batch_size,outfile):
     
     return data
 
+if __name__ == "__main__":
+    BATCH_SIZE = 32
+    outfile = './data.pt'
+
+    DATA = save_data(data_path='../data/train_data.csv', batch_size=BATCH_SIZE,outfile=outfile)
+    
+    pass
