2f6c64b07d846a3e961ed4472920100611e7ac66
Pedja-Djape <pmuratov1015@gmail.com>
2023-01-10 11:15:05 -0500

    added test functionality

--- server_new.py
+++ server_new.py
@@ -1,6 +1,6 @@
 import strategy_new as stgy
 import pickle
-from utils import ShoppersDataset,save_data
+from utils import ShoppersDataset,load_datasets,save_data
 import time
 
 from torch.optim import Adam
@@ -11,23 +11,18 @@ import flwr as fl
 if __name__ == "__main__":
     BATCH_SIZE = 32
     NUM_CLIENTS = 3
-    NUM_ROUNDS = 5000
+    NUM_ROUNDS = 30008
     outfile = './data.pt'
     infile = './data.pt'
 
     
-    DATA = save_data(data_path='../data/train_data.csv', batch_size=BATCH_SIZE, outfile=outfile)
-
-    if 'batch_size' not in DATA or DATA['batch_size'] != BATCH_SIZE:
-        print(f"Saving data with new batch_size: {BATCH_SIZE}")
-        DATA = save_data(data_path='../data/train_data.csv', batch_size=BATCH_SIZE, outfile=outfile)
-    
-    
+    DATA = save_data(data_path='../data/train_data.csv', batch_size=BATCH_SIZE,outfile=outfile)
     strategy = stgy.SplitVFL(
         num_clients=NUM_CLIENTS, 
         batch_size=BATCH_SIZE, 
         dim_input= 18, # 6 outputs for three clients
-        labels = DATA['labels']
+        train_labels = DATA['train_labels'],
+        test_labels=DATA['test_labels']
     )
 
     fl.server.start_server(
@@ -37,3 +32,14 @@ if __name__ == "__main__":
         ),
         strategy = strategy
     )
+    
+    import matplotlib.pyplot as plt 
+
+    fig, axs = plt.subplots(1,2,sharey=True)
+    
+    axs[0].plot(strategy.train_acc,label='Train Accuracy')
+    axs[1].plot(strategy.test_acc, label='Test Accuracy')
+    axs[1].plot(strategy.test_f1,label='Test F1-Score')
+    axs[0].legend()
+    axs[1].legend()
+    plt.show()
