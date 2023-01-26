ec9956612e2d71ef8f22285dda6db2f7dced6d61
Pedja-Djape <pmuratov1015@gmail.com>
2023-01-26 14:54:46 -0500

    added input args

--- run.sh
+++ run.sh
@@ -2,9 +2,14 @@
 set -e
 cd "$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"/
 
+BATCH_SIZE=32
+NUM_CLIENTS=3
+DATA_OUTPUT='./data.pt'
+NUM_ROUNDS=5000
+TRAIN_DATA='../data/tmp_train_data.csv'
 
 echo "Starting server"
-python3 server_new.py &
+python3 server_new.py -bs $BATCH_SIZE -n $NUM_CLIENTS -do $DATA_OUTPUT -nr $NUM_CLIENTS -f $TRAIN_DATA &
 sleep 3  # Sleep for 3s to give the server enough time to start
 
 # Start clients with given IDs
@@ -20,4 +25,4 @@ trap "trap - SIGTERM && kill -- -$$" SIGINT SIGTERM
 wait
 
 # Test VFL system
-python3 compute_test_metrics.py
+python3 compute_test_metrics.py -bs $BATCH_SIZE -n $NUM_CLIENTS -d $DATA_OUTPUT
